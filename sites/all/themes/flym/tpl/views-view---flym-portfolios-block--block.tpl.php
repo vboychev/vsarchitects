175c0c0ada809d894f7c4aae11f84233d39c18f2
Vasil Boychev <vassil.boychev@ffwagency.com>
2016-05-05 18:47:36 +0300

    fixed front page categories links

--- sites/all/themes/flym/tpl/views-view---flym-portfolios-block--block.tpl.php
+++ sites/all/themes/flym/tpl/views-view---flym-portfolios-block--block.tpl.php
@@ -1,15 +1,14 @@
 <div class="w-hidden-tiny w-clearfix div-social">
     <div class="w-clearfix filters">
         <ul class="w-list-unstyled filter-list">
-            <li class="filter-iterm"><a class="filter" href="#" data-filter="all">All</a>
+            <li class="filter-iterm"><?php print l(t('All'), NULL, array('attributes' => array('data-filter' => 'all', 'class' => array('filter')), 'fragment' => FALSE)); ?>
                 <?php
                 $name = 'project_categories';
                 $myvoc = taxonomy_vocabulary_machine_name_load($name);
                 $tree = taxonomy_get_tree($myvoc->vid);
                 foreach ($tree as $term) {
                   $term = i18n_taxonomy_localize_terms($term);
-                    print'<li class="filter-iterm"><a class="filter" href="#" data-filter=".' . $term->tid . '">' . $term->name . '</a>
-            </li>';
+                  print '<li class="filter-iterm">' . l($term->name, NULL, array('attributes' => array('data-filter' => '.' . $term->tid, 'class' => array('filter')), 'fragment' => FALSE)) . '</li>';
                 }
                 ?>
             <li class="filter-iterm">
